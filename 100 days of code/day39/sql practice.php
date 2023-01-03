3208d5ed9fc17a6a0b9aa913d4d5c484af60f9f6
Koder-Dev <116238476+Xtha-Sunil@users.noreply.github.com>
2023-01-03 09:40:28 +0545

    Create sql practice.php

---
+++ 100 days of code/day39/sql practice.php
@@ -0,0 +1,10 @@
+<?php
+"Today i learned about SQL by doing questions of 
+SQL in app called SQL Practice Pro";
+  
+"App used :- " . https://play.google.com/store/apps/details?id=randomappsinc.com.sqlpracticeplus
+
+
+
+
+?>
